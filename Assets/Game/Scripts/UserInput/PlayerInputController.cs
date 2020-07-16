43118c002123f8766b10a1682919bd9625685fc6
Nils <its.just.nils@gmail.com>
2020-07-16 16:12:10 +0200

    add debug keyboard input

--- Assets/Game/Scripts/UserInput/PlayerInputController.cs
+++ Assets/Game/Scripts/UserInput/PlayerInputController.cs
@@ -37,6 +37,41 @@ namespace Game.UserInput
             jumpButton.ReleasedEvent -= OnButtonReleased;
         }
 
+        #if UNITY_EDITOR
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                OnButtonDown(PlayerInputAction.MoveLeft);
+            }
+
+            if (Input.GetKeyUp(KeyCode.A))
+            {
+                OnButtonReleased(PlayerInputAction.MoveLeft);
+            }
+
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                OnButtonDown(PlayerInputAction.Jump);
+            }
+
+            if (Input.GetKeyUp(KeyCode.W))
+            {
+                OnButtonReleased(PlayerInputAction.Jump);
+            }
+
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                OnButtonDown(PlayerInputAction.MoveRight);
+            }
+
+            if (Input.GetKeyUp(KeyCode.D))
+            {
+                OnButtonReleased(PlayerInputAction.MoveRight);
+            }
+        }
+        #endif
+
         private void OnButtonDown(PlayerInputAction playerInputAction)
         {
             ActionEvent?.Invoke(playerInputAction, ActionState.Start);
