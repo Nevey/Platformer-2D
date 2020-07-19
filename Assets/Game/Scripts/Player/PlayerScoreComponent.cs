bc15bbb129988c392e753a29570bcd44f3868ca8
Nils <its.just.nils@gmail.com>
2020-07-19 12:54:44 +0200

    add very simple scoring based on player's health

---
+++ Assets/Game/Scripts/Player/PlayerScoreComponent.cs
@@ -0,0 +1,35 @@
+﻿using Game.DI;
+using Game.Health;
+using Game.Scoring;
+using UnityEngine;
+
+namespace Game.Player
+{
+    [RequireComponent(typeof(HealthComponent))]
+    public class PlayerScoreComponent : DIBehaviour
+    {
+        [Inject] private ScoreController scoreController;
+
+        private HealthComponent healthComponent;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            healthComponent = GetComponent<HealthComponent>();
+            healthComponent.HealthUpdatedEvent += OnHealthUpdated;
+        }
+
+        protected override void OnDestroy()
+        {
+            healthComponent.HealthUpdatedEvent -= OnHealthUpdated;
+
+            base.OnDestroy();
+        }
+
+        private void OnHealthUpdated(int health)
+        {
+            scoreController.SetScore(health);
+        }
+    }
+}
